1242b1892c633a885e31dde40949e56a4400684a
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-08-26 23:40:25 -0300

    feat: criação e implementação da tabela de status da comida.

---
+++ src/test/java/br/com/laplaza/LaPlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.laplaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaPlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
